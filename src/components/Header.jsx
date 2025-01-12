60ac04b184d9cc29db38b02596037d62b3e59b05
Shanthinath Shedabale <102481278+shanthi1710@users.noreply.github.com>
2025-01-12 10:39:51 +0530

    refactored header.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,26 @@ import { LOGO_URL } from "../utils/constants";
 const Header = () => {
   return (
     <header className="bg-white shadow-md">
-      <div className="container mx-auto flex items-center justify-between py-4 px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto flex flex-wrap items-center justify-between py-4 px-4 sm:px-6 lg:px-8">
         {/* Logo Section */}
-        <div>
-          <img src={LOGO_URL} alt="BrainStromFood Logo" className="w-3/12" />
+        <div className="w-full sm:w-auto flex justify-center sm:justify-start mb-4 sm:mb-0">
+          <img
+            src={LOGO_URL}
+            alt="BrainStromFood Logo"
+            className="h-10 sm:h-12"
+          />
         </div>
 
         {/* Search Bar */}
-        <div className="w-full max-w-md flex items-center">
+        <div className="w-full sm:w-auto flex flex-grow sm:flex-grow-0 max-w-md items-center">
           <input
             type="text"
             placeholder="Search for dishes..."
-            className="flex-grow p-2 m-2 pl-4 border border-black rounded-lg"
+            className="flex-grow p-2 pl-4 border border-black rounded-lg"
           />
           <button
             type="button"
-            className="p-2 bg-blue-500 text-white border rounded-lg border-black hover:border-blue-700 hover:bg-blue-600"
+            className="p-2 ml-2 bg-blue-500 text-white border rounded-lg border-black hover:border-blue-700 hover:bg-blue-600"
           >
             Search
           </button>
